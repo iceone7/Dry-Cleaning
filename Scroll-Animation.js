333f0c82b518f48e97e1b0bb81b27bfd33499112
pavlik <107913419+iceone7@users.noreply.github.com>
2024-10-16 14:20:09 +0400

    first commit

---
+++ Scroll-Animation.js
@@ -0,0 +1,5 @@
+ScrollReveal({ distance: '10px' });
+ScrollReveal().reveal(".left-header", { origin: 'left' });
+ScrollReveal().reveal(".service-inforamtion", { interval: 200 });
+ScrollReveal().reveal(".progress-inforamtion", { interval: 100 });
+
